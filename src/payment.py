0d024a16fd9d9c847f6a52597b73f2f3115f5843
mjcjc <mjcjc@naver.com>
2024-06-06 12:02:37 +0900

    2차pw까지 된 상태 결제만 되게 하면 됨

--- src/payment.py
+++ src/payment.py
@@ -1,34 +1,37 @@
-# from selenium import webdriver
-# from selenium.webdriver.chrome.options import Options
-# from selenium.webdriver.common.by import By
-# from selenium.webdriver.support.ui import WebDriverWait
-# from selenium.webdriver.support import expected_conditions as EC
+from selenium import webdriver
+from bs4 import BeautifulSoup
+import requests
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import time
 
-# # 웹 드라이버를 초기화합니다. (Chrome을 사용한다고 가정)
-# chrome_options = Options()
-# chrome_options.add_experimental_option("detach", True)
-# driver = webdriver.Chrome(options=chrome_options) 
-# # 웹페이지를 엽니다.
-# url = "https://booking.naver.com/booking/13/bizes/780595/items/5428141?theme=place&area=ple"
-# driver.get(url)
 
-# # 날짜를 선택합니다. (예: 5일 후)
-# wait = WebDriverWait(driver, 15)
-# calendar = wait.until(EC.presence_of_element_located((By.CLASS_NAME, 'calendar_body')))
-# days = calendar.find_elements(By.CSS_SELECTOR, 'td')
-# days[3].click()  # 5일 후를 선택합니다.
+# TODO
+#여기서는 결제 하는 과정을 해야함.
+#
+chrome_options = Options()
+chrome_options.add_experimental_option("detach", True)
+driver = webdriver.Chrome(options=chrome_options) # 크롬 드라이버의 경로를 설정
+headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'}
 
-# # 'calendar_area'와 'time_area' 클래스를 찾습니다.
-# section_content = wait.until(EC.presence_of_element_located((By.CLASS_NAME, 'section_content')))
-# calendar_area = wait.until(EC.presence_of_element_located((By.CLASS_NAME, 'calendar_area')))
-# time_area = wait.until(EC.presence_of_element_located((By.CLASS_NAME, 'time_area')))
+#일준 
+url = "https://booking.naver.com/booking/13/bizes/780595/items/5428141?theme=place&area=ple"
+data = requests.get(url,headers=headers)
+soup = BeautifulSoup(data.text, 'html.parser')
+driver.get(url)
 
-# # 각 영역의 텍스트를 추출합니다.
-# calendar_text = calendar_area.text
-# time_text = time_area.text
 
-# print("Calendar Area Text: ", calendar_text)
-# print("Time Area Text: ", time_text)
+time.sleep(3)
 
-# # 웹 드라이버를 종료합니다.
-# driver.quit()
+html = driver.page_source
+soup = BeautifulSoup(html, 'html.parser')
+
+dates = []
+
+
+
+driver.quit()
+
+
--- tiki.py
+++ tiki.py
@@ -17,8 +17,10 @@ getimes = None
 getdays = None
 getid = None
 getpw = None
+getpw2 = None
 #헤어숍 화면
 #여기에다가 아이디 비밀번호 2차 비밀번호 넣는거 해야함.
+#일단 성공 했는데 
 def hair_frame():
     frame2.lift()
     prev_frame = Frame(root, relief='solid')
@@ -32,14 +34,40 @@ def hair_frame():
     getdays = comboboxtime.get()
     comboboxtime.set("시간 클릭")
     getimes = comboboxtime.get()
+    
 
-    #텍스트 박스 2개 안뜸 수정 해야함.
-    idText = Entry(root, width=10)
-    idText.place(x=100, y=140)  # place 메서드를 사용하여 위치 지정
-    getid = idText.get()
-    pwText = Entry(root, width=10)
-    pwText.place(x=100, y=160)
-    getpw = pwText.get()
+    #수정 끝났고 이제 값 받아서 payment에 넘겨서 결제하는거 해야함.
+    # idText = Entry(root, width=10)
+    # idText.place(x=100, y=140)  # place 메서드를 사용하여 위치 지정
+    # idText.insert(0,"아이디")
+    # getid = idText.get()
+    # pwText = Entry(root, width=10)
+    # pwText.place(x=100, y=160)
+    # pwText.insert(0,"비밀번호")
+    # getpw = pwText.get()
+    # pw2Text = Entry(root,width=10)
+    # pw2Text.place(x=100, y=180)
+    # pw2Text.insert(0,"2차 비밀번호")
+    # getpw2 = pw2Text.get()
+
+    #이거 참고하면서 내 코드 수정하면 될 듯 show기능 좋은 거 같다.
+    label = ttk.Label(root, text="Login :")
+    label.place(x=90, y=160)
+
+    idText = ttk.Entry(root)
+    idText.place(x=100, y=160)
+
+    label = ttk.Label(root, text="Password :")
+    label.place(x=90, y=180)
+
+    pwText = ttk.Entry(root, show="*")
+    pwText.place(x=100, y=180)
+
+    label = ttk.Label(root, text="2차 비밀번호 :")
+    label.place(x=90, y=200)
+
+    pw2Text = ttk.Entry(root, show="*")
+    pw2Text.place(x=100, y=200)
     
 # 메인화면
 def main_frame():
@@ -64,7 +92,7 @@ frame2.place(x=0, y=0, width=400, height=250)
 frame1.place(x=0, y=0, width=400, height=250)  # 이 프레임이 가장 먼저 나타남
 btn_frame.place(x=0, y=100, width=400, height=350)  # 밑 바닥에 이전과 다음이 있을 버튼 영역
 
-Label(frame3, text="티켓예매", font=('consolas', 20)).pack()
+#Label(frame3, text="티켓예매", font=('consolas', 20)).pack()
 Label(frame2, text="헤어숍", font=('consolas', 20)).pack()
 Label(frame1, text="메인", font=('consolas', 20)).pack()
 
